dfdb4c2d9ae2907be11df3f8fee43c83bca1c73f
Erico Silva <112210712+dmsdy@users.noreply.github.com>
2024-10-09 22:33:57 -0300

    Update PapelMoeda.java
    Comentários

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -1,23 +1,41 @@
 package br.calebe.ticketmachine.core;
 
 /**
- *
- * @author Calebe de Paula Bianchini
+ * Classe que representa uma cédula de dinheiro.
  */
 public class PapelMoeda {
 
+    // Valor da cédula (ex: 2, 5, 10, 20, 50, 100)
     protected int valor;
+
+    // Quantidade de cédulas desse valor
     protected int quantidade;
 
+    /**
+     * Construtor que inicializa o valor e a quantidade de cédulas.
+     * 
+     * @param valor Valor da cédula.
+     * @param quantidade Quantidade de cédulas.
+     */
     public PapelMoeda(int valor, int quantidade) {
         this.valor = valor;
         this.quantidade = quantidade;
     }
 
+    /**
+     * Retorna o valor da cédula.
+     * 
+     * @return Valor da cédula.
+     */
     public int getValor() {
         return valor;
     }
 
+    /**
+     * Retorna a quantidade de cédulas.
+     * 
+     * @return Quantidade de cédulas.
+     */
     public int getQuantidade() {
         return quantidade;
     }
